be183c5d15e17fc0daeca3d773b7b25e76f02c54
AlexCanadas <147659337+AlexCanadas@users.noreply.github.com>
2024-10-09 09:10:17 +0200

    ok

---
+++ Programación multimedia y dispositivos móviles/Inicio/src/Ejercicios T1 Kotlin.kt
@@ -0,0 +1,12 @@
+class `Ejercicios T1 Kotlin` {
+    // 1. Escribe un programa que pida al usuario dos números enteros y calcule su suma.
+    // 2. Escribe un programa que pida el radio de un círculo y calcule su área. Utiliza la fórmula Área = π * radio^2.
+    // 3. Escribe un programa que convierta grados Celsius a Fahrenheit. Usa la fórmula: F = C * 9/5 + 32.
+    // 4. Escribe un programa que lea un número entero y determine si es par o impar (el if es igual que en Java)
+    // 5. Escribe un programa que lea dos números enteros y determine cuál es mayor.
+    // 6. Escribe un programa que calcule el factorial de un número ingresado por el usuario.
+    // 7. Escribe un programa que simule una calculadora básica. El programa debe pedir al usuario dos números y una operación (suma, resta, multiplicación, división) y mostrar el resultado.
+    // 8. Escribe un programa que determine si un número ingresado por el usuario es primo
+    // 9. Escribe un programa que maneje información de usuarios, donde algunos campos pueden ser opcionales (es decir, pueden ser null). En el programa, ni el apellido ni la edad pueden ser null.
+         // Si no se proporciona la edad, debes manejar un valor por defecto. Crear una función que reciba el nombre, apellido y edad y devuelva un mensaje con la información completa del usuario. Maneja de forma segura los posibles nulos
+}
