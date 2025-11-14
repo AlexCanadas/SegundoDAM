59c79c65fc5be86028c6427b4de1eab5c8ac0557
AlexCanadas <147659337+AlexCanadas@users.noreply.github.com>
2025-11-14 18:39:29 +0100

    ok

--- ProgramacionDeServiciosYProcesos/ActividadEvaluable1PSP/src/NumImpares.java
+++ ProgramacionDeServiciosYProcesos/ActividadEvaluable1PSP/src/NumImpares.java
@@ -4,7 +4,7 @@ import java.util.concurrent.Semaphore;
 public class NumImpares extends Thread {
 	private String nombreHilo;
 	private int tiempo;
-	private static int sumaTotal = 0; // Variable estática para la suma total
+	private static int sumaTotal = 0; // Creamos variable estática para la suma total
 	private static Semaphore semaforo = new Semaphore(1); // Solo dejamos un hilo
 
 	public NumImpares(String nombreHilo, int tiempo) {
@@ -20,14 +20,13 @@ public class NumImpares extends Thread {
 			for (int i = 1; i <= 10; i += 2) {
 				System.out.println(nombreHilo + " ---> Número: " + i);
 				suma += i;
-				Thread.sleep(tiempo); // Pausa según el tiempo especificado
+				Thread.sleep(tiempo); // Pausa según el tiempo quew se introduzca por consola
 			}
 			System.out.println("Suma de números impares en " + nombreHilo + ": " + suma);
-			System.out.println();
 
-			// El uso de semáforos asegura que solo un hilo pueda acceder y modificar la
-			// variable compartida sumaTotal a la vez
-			semaforo.acquire(); // Adquirir permiso del semáforo
+			// Creamos semáforo para que solo un hilo pueda modificar la variable sumaTotal
+			// a la vez
+			semaforo.acquire(); // Pedir permiso del semáforo
 			try {
 				sumaTotal += suma;
 			} finally {
@@ -35,7 +34,6 @@ public class NumImpares extends Thread {
 			}
 
 			System.out.println(nombreHilo + " ha terminado.");
-			System.out.println();
 
 		} catch (InterruptedException e) {
 			e.printStackTrace();
@@ -45,7 +43,6 @@ public class NumImpares extends Thread {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Introduce el tiempo de pausa para los hilos de números impares (en milisegundos): ");
-		System.out.println();
 		int tiempo = sc.nextInt();
 
 		System.out.println("Ejecutando hilos de números impares:");
--- ProgramacionDeServiciosYProcesos/ActividadEvaluable1PSP/src/NumPares.java
+++ ProgramacionDeServiciosYProcesos/ActividadEvaluable1PSP/src/NumPares.java
@@ -5,7 +5,7 @@ public class NumPares extends Thread {
 	private String nombreHilo;
 	private int tiempo;
 	private static int sumaTotal = 0; // Variable estática para la suma total
-	private static Semaphore semaforo = new Semaphore(1); // Semáforo limitado a 1
+	private static Semaphore semaforo = new Semaphore(1); // Solo dejamos un hilo
 
 	public NumPares(String nombreHilo, int tiempo) {
 		this.nombreHilo = nombreHilo;
@@ -20,14 +20,13 @@ public class NumPares extends Thread {
 			for (int i = 2; i <= 10; i += 2) {
 				System.out.println(nombreHilo + " ---> Número: " + i);
 				suma += i;
-				Thread.sleep(tiempo); // Pausa según el tiempo especificado
+				Thread.sleep(tiempo); // Pausa según el tiempo quew se introduzca por consola
 			}
 			System.out.println("Suma de números pares en " + nombreHilo + ": " + suma);
-			System.out.println();
 
-			// El uso de semáforos asegura que solo un hilo pueda acceder y modificar la
-			// variable compartida sumaTotal a la vez
-			semaforo.acquire(); // Adquirir permiso del semáforo
+			// Creamos semáforo para que solo un hilo pueda modificar la variable sumaTotal
+			// a la vez
+			semaforo.acquire(); // Pedir permiso del semáforo
 			try {
 				sumaTotal += suma;
 			} finally {
@@ -35,7 +34,6 @@ public class NumPares extends Thread {
 			}
 
 			System.out.println(nombreHilo + " ha terminado.");
-			System.out.println();
 
 		} catch (InterruptedException e) {
 			e.printStackTrace();
@@ -45,7 +43,6 @@ public class NumPares extends Thread {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Introduce el tiempo de pausa para los hilos de números pares (en milisegundos): ");
-		System.out.println();
 		int tiempo = sc.nextInt();
 
 		System.out.println("Ejecutando hilos de números pares:");
@@ -58,8 +55,7 @@ public class NumPares extends Thread {
 		hilo2Par.start();
 
 		try {
-			// Esperar a que ambos hilos terminen. El programa principal espera a que
-			// terminen antes de mostrar la sumaTotal.
+			// Esperar a que ambos hilos terminen
 			hilo1Par.join();
 			hilo2Par.join();
 
